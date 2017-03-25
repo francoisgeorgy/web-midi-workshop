a233ba6f2e2b504ba296673a89a591cdbbf87158
François Georgy <francois.georgy@gmail.com>
2017-03-25 18:12:12 +0100

    Added spectre.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { version } from 'inferno';
 import Component from 'inferno-component';
 import Logo from './logo';
+import '../node_modules/spectre.css/dist/spectre.min.css';
 import './App.css';
 
 class App extends Component {
@@ -14,6 +15,15 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+            <h1>Test with spectre.css</h1>
+            <div class="form-group">
+                <label class="form-switch">
+                    <input type="checkbox" />
+                    <i class="form-icon"></i> spectree.css switch
+                </label>
+            </div>
+        </div>
       </div>
     );
   }
